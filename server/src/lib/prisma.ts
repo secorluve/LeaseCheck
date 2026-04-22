3a0246e953e38ad33bda69f123b58118615e78b3
secorluve <hvan.ru354@gmail.com>
2026-04-22 20:37:26 +0500

    Configure Prisma v7 PostgreSQL adapter

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,7 +1,12 @@
 import prismaClientPkg from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 import type { PrismaClient as PrismaClientType } from '@prisma/client'
+import { env } from '../config/env'
 
 const { PrismaClient } = prismaClientPkg
+const adapter = new PrismaPg({
+  connectionString: env.DATABASE_URL,
+})
 
 const globalForPrisma = globalThis as typeof globalThis & {
   __leasecheckPrisma?: PrismaClientType
@@ -10,6 +15,7 @@ const globalForPrisma = globalThis as typeof globalThis & {
 export const prisma =
   globalForPrisma.__leasecheckPrisma ??
   new PrismaClient({
+    adapter,
     log: ['warn', 'error'],
   })
 
